3e4e7b65e193fe7a947f7d20d60b4d5288970ac9
Tianzong Cheng <oierprime@outlook.com>
2026-03-27 19:40:34 +0800

    Initial Commit

---
+++ Indoor3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Indoor3D
+//
+//  Created by 程天纵 on 2026/3/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Indoor3D/Indoor3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Indoor3DApp.swift
+//  Indoor3D
+//
+//  Created by 程天纵 on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct Indoor3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
